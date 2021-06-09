6b30231aead827a9d2d1b4da73daeb593383092b
Mary Toth <maryarylynn@gmail.com>
2021-06-09 12:25:49 -0400

    put main list of pets in separate file from app, removed petdetails page

---
+++ src/ListOfPetsPage.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+export function ListOfPetsPage() {
+  const [pet, setPet] = useState([])
+  const [name, setName] = useState('')
+
+  useEffect(function () {
+    async function loadThePets() {
+      const response = await axios.get(
+        'https://tamagotchiapi.herokuapp.com/api/Pets'
+      )
+      setPet(response.data)
+    }
+    loadThePets()
+  }, [])
+
+  async function handleCreateAPet(event) {
+    event.preventDefault()
+    const response = await axios.post(
+      'https://tamagotchiapi.herokuapp.com/api/Pets',
+      { name: name }
+    )
+    const refreshPets = await axios.get(
+      'https://tamagotchiapi.herokuapp.com/api/Pets'
+    )
+    setPet(refreshPets.data)
+    setName('')
+  }
+  return (
+    <>
+      <nav>
+        <ul>
+          {Object.entries(pet).map(([petCode, petDetails]) => {
+            return (
+              <li key={petDetails.id}>
+                {' '}
+                <Link to={`/pets/${petDetails.id}`}>{petDetails.name}</Link>
+              </li>
+            )
+          })}
+          <form>
+            <h4>Create a New Pet</h4>
+            <label htmlFor="text1">Name </label>
+            <input
+              type="text"
+              placeholder="Enter the pet's name"
+              value={name}
+              onChange={function (event) {
+                setName(event.target.value)
+              }}
+            ></input>
+            <button onClick={handleCreateAPet}>Create</button>
+          </form>
+        </ul>
+      </nav>
+    </>
+  )
+}
